6575d42e66f5e74af47da3fe77a5f538bb97aa5d
devsayanR <sayanrakshit.prof@gmail.com>
2024-12-25 15:47:02 +0530

    feat(docs): add new documentation for supervision metrics and utilities

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -55,6 +55,97 @@ export const ROUTES: EachRoute[] = [
       { title: "Tailwind", href: "/tailwind" },
       { title: "Web Socket", href: "/websocket", },
     ],
+  },
+  {
+    title: "Supervision",
+    href: "/supervision",
+    noLink: true,
+    items: [
+      { title: "Classification", href: "/classification" },
+      {
+        title: "Datasets",
+        href: "/datasets",
+        noLink: true,
+        items: [
+          { title: "Core", href: "/core" },
+          { title: "Utils", href: "/utils" },
+        ]
+      },
+      {
+        title: "Detection",
+        href: "/detection",
+        noLink: true,
+        items: [
+          { title: "Annotators", href: "/annotators" },
+          { title: "Core", href: "/core" },
+          { title: "Double Delection Filter", href: "/double_detection_filter" },
+          { title: "Metrics", href: "/metrics" },
+          {
+            title: "Tools",
+            href: "/tools",
+            noLink: true,
+            items: [
+              { title: "Inference Slicer", href: "/inference_slicer" },
+              { title: "Line Zone", href: "/line_zone" },
+              { title: "Polygon Zone", href: "/polygon_zone" },
+              { title: "Save Detections", href: "/save_detections" },
+              { title: "Smoother", href: "/smoother" },
+            ]
+          },
+          { title: "Utils", href: "/utils" },
+        ]
+      },
+      {
+        title: "How to",
+        href: "/how_to",
+        noLink: true,
+        items: [
+          { title: "Benchmark Model", href: "/benchmark_a_model" },
+          { title: "Detect and Annotate", href: "/detect_and_annotate" },
+          { title: "Detect Small Objects", href: "/detect_small_objects" },
+          { title: "Filter Detections", href: "/filter_detections" },
+          { title: "Process Datasets", href: "/process_datasets" },
+          { title: "Save Detections", href: "/save_detections" },
+          { title: "Track Objects", href: "/track_objects" },
+        ]
+      },
+      {
+        title: "Key Point",
+        href: "/keypoint",
+        noLink: true,
+        items: [
+          { title: "Annotators", href: "/annotators" },
+          { title: "Core", href: "/core" },
+        ]
+      },
+      {
+        title: "Metrics",
+        href: "/metrics",
+        noLink: true,
+        items: [
+          { title: "Common Values", href: "/common_values" },
+          { title: "F1 Score", href: "/f1_score" },
+          { title: "Mean Average precision", href: "/mean_average_precision" },
+          { title: "Mean Average Recall", href: "/mean_average_recall" },
+          { title: "Precision", href: "/precision" },
+          { title: "recall", href: "/recall" },
+        ]
+      },
+      {
+        title: "Utils",
+        href: "/utils",
+        noLink: true,
+        items: [
+          { title: "Draw", href: "/draw" },
+          { title: "File", href: "/file" },
+          { title: "Geometry", href: "/geometry" },
+          { title: "Image", href: "/image" },
+          { title: "Iterables", href: "/iterables" },
+          { title: "Notebook", href: "/notebook" },
+          { title: "Video", href: "/video" },
+        ]
+      },
+    ],
   }
 ];
 
